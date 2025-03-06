9026fadb7503768437868a9dd37f04c73d631af6
Mahdi Hasan Mehedi <129614659+Mahdi767@users.noreply.github.com>
2025-03-06 13:21:02 +0600

    Add files via upload

---
+++ Word Counter/src/App.java
@@ -0,0 +1,21 @@
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+    JFrame frame = new JFrame("Word Counter");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(600, 400);
+
+
+
+
+
+
+
+
+
+    
+    frame.setVisible(true);
+
+    }
+}
